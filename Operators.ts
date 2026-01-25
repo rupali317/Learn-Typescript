9d67644bab97d4d8b522d119e9d66b757d8e815c
rupali317 <rupali317@gmail.com>
2026-01-25 23:40:29 +0800

    Operators experiment

---
+++ Operators.ts
@@ -0,0 +1,31 @@
+let num;
+
+num = "2" + "2"; // "22"
+num = 2 + 2; // 4
+
+//These work in JS
+// num = 2 - "2"; // 0
+// num = "2" * 2; // 4
+// num = 2 / "2"; // 1
+// num = "2" % "2"; // 0
+
+console.log(2 === 2); // T
+console.log(2 !== 2); // F
+console.log(2 > 2); // F
+console.log(2 >= 2); // T
+console.log(2 < 2); // F
+console.log(2 <= 2); // T
+
+// console.log("2" === 2); // F
+// console.log("2" !== 2); // F
+// console.log("2" > 2); // F
+// console.log("2" >= 2); // T
+// console.log("2" < 2); // F
+// console.log("2" <= 2); // T
+
+console.log("2" === "2"); // T
+console.log("2" !== "2"); // F
+console.log("2" > "2"); // F
+console.log("2" >= "2"); // T
+console.log("2" < "2"); // F
+console.log("2" <= "2"); // T
